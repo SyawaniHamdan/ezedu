f8c11217740eec00371928edb175f89bca46f158
Syawani <syawanihamdan96@gmail.com>
2022-01-21 03:19:49 +0800

    UI login page tutor done so far - Syawani

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 //import 'package:ez_edu/templates/templateMenu.dart';
 import 'package:ezedu/screens/home/main/widgets/home_main_screen.dart';
+import 'package:ezedu/screens/tutor/login/widgets/tutor_login_main_screen.dart';
 import 'package:ezedu/templates/templateMenu.dart';
 import 'package:flutter/material.dart';
 
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: primaryColor,
       ),
-      home: HomeMainScreen(),
+      home: TutorLoginMainScreen(),
     );
   }
 }
---
+++ lib/screens/tutor/login/widgets/tutor_login_main_screen.dart
@@ -0,0 +1,12 @@
+import 'package:ezedu/screens/tutor/login/widgets/tutor_login_main_body.dart';
+import 'package:flutter/material.dart';
+
+
+class TutorLoginMainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: TutorLoginMainBody(),
+    );
+  }
+}
