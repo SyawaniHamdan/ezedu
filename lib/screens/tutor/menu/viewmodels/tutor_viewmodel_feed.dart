de878a2c0b1613b68d6e5ab7540eee2c1fde47b7
Syawani <syawanihamdan96@gmail.com>
2022-01-31 11:34:42 +0800

    Changes feed viewmodel

--- lib/screens/tutor/menu/viewmodels/tutor_viewmodel_feed.dart
+++ lib/screens/tutor/menu/viewmodels/tutor_viewmodel_feed.dart
@@ -1,28 +1,35 @@
 import 'dart:io';
 import 'package:ezedu/app/locator.dart';
 import 'package:ezedu/models/subject.dart';
+import 'package:ezedu/models/tutor.dart';
 import 'package:ezedu/screens/viewmodel.dart';
 import 'package:ezedu/services/authentication_service.dart';
 import 'package:ezedu/services/subject_service.dart';
+import 'package:ezedu/services/tutor_service.dart';
 
 class TutorFeedViewModel extends ViewModel {
   final AuthenticationService _authenticationService =
     locator<AuthenticationService>();
 
   final SubjectService _subjectService = locator<SubjectService>();
+  final TutorService _tutorService = locator<TutorService>();
   
   bool empty = false;
 
-  late List<Subject> _subjectList;
+  List<Subject>? _subjectList;
   get subjectList => _subjectList;
+
+  List<Tutor>? _tutorList;
+  get tutorList => _tutorList;
 
   Future initialise() async {
     setBusy(true);
 
-    _subjectList =
-        await _subjectService.getSubjectByTutorId(currentTutor.id!);
+    _subjectList = await _subjectService.getSubjectByTutorId(currentTutor.id!);
 
-    if (_subjectList.length == 0) empty = true;
+    _tutorList = (await _tutorService.getTutor()) as List<Tutor>?;
+
+    if (_subjectList?.length == 0) empty = true;
 
     setBusy(false);
   }
@@ -42,5 +49,11 @@ class TutorFeedViewModel extends ViewModel {
 
     setBusy(false);
   }
+
+  Tutor getSubjectTutor(String tutorId) =>
+      tutorList.firstWhere((tutor) => tutor.id == tutorId);
+  
+  Subject getSubject(String subjectId) =>
+      subjectList.firstWhere((subject) => subject.id == subjectId);
 
 }
