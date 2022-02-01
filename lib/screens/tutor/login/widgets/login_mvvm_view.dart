d1eca1b3d32f1319bbbfdda463242ea8a4117967
Syawani <syawanihamdan96@gmail.com>
2022-02-01 16:15:59 +0800

    Adding keyword student/tutor on login page

--- lib/screens/tutor/login/widgets/login_mvvm_view.dart
+++ lib/screens/tutor/login/widgets/login_mvvm_view.dart
@@ -35,7 +35,7 @@ class TutorLoginViewState extends State<TutorLoginView> {
                       alignment: Alignment.centerLeft,
                       padding: const EdgeInsets.symmetric(horizontal: 40),
                       child: const Text(
-                        "LOGIN",
+                        "TUTOR LOGIN",
                         style: TextStyle(
                             fontWeight: FontWeight.bold,
                             color: Color(0XFF006064),
